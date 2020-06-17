8ef044cb5a022d13e59491ab6b1c1a7565f355a8
NHAT100 <buitruongnhatlm@gmail.com>
2020-06-18 06:41:44 +0700

    course 2

--- QuanLySinhVienTotNghiep/View/CoursePage.xaml.cs
+++ QuanLySinhVienTotNghiep/View/CoursePage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DAL;
+using DTO;
 
 namespace QuanLySinhVienTotNghiep.View
 {
@@ -23,7 +26,10 @@ namespace QuanLySinhVienTotNghiep.View
         public CoursePage()
         {
             InitializeComponent();
+            LoadDataToGrid();
         }
+
+
 
         private void DtgNganh_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
@@ -47,22 +53,121 @@ namespace QuanLySinhVienTotNghiep.View
 
         private void DtgKhoa_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            DataGrid _dataGrid = sender as DataGrid;
+            DataRowView _dataRow = _dataGrid.SelectedItem as DataRowView;
 
+            if (_dataGrid != null && _dataRow != null)
+            {
+                txtMaKhoa.Text = _dataRow["MaKhoa"].ToString();
+                txtTenKhoa.Text = _dataRow["TenKhoa"].ToString();
+                txtGhiChuKhoa.Text = _dataRow["GhiChu"].ToString();
+            }
         }
 
         private void BtnThemKhoa_Click(object sender, RoutedEventArgs e)
         {
+            int _makhoa = Convert.ToInt32(txtMaKhoa.Text);
+            string _tenkhoa = txtTenKhoa.Text;
+            string _ghichu = txtGhiChuKhoa.Text;
+
+            AddCourse(_makhoa, _tenkhoa, _ghichu);
+            ClearDataTextbox(txtMaKhoa, txtTenKhoa, txtGhiChuKhoa);
 
         }
 
         private void BtnCapNhatKhoa_Click(object sender, RoutedEventArgs e)
         {
+            int _idkhoa = 0;
+            DataRowView _rowCurrent = dtgKhoa.SelectedItem as DataRowView;
 
+            if (_rowCurrent != null)
+            {
+                _idkhoa = Convert.ToInt32(_rowCurrent["IDKhoa"].ToString());
+            }
+
+            int _maKhoa = Convert.ToInt32(txtMaKhoa.Text);
+            string _tenkhoa = txtTenKhoa.Text;
+            string _ghichu = txtGhiChuKhoa.Text;
+
+            EditCourse(_idkhoa,_maKhoa ,_tenkhoa, _ghichu);
+            ClearDataTextbox(txtMaKhoa,txtTenKhoa,txtGhiChuKhoa);
         }
 
         private void BtnXoaKhoa_Click(object sender, RoutedEventArgs e)
         {
+            int _idkhoa = 0;
+            DataRowView _rowCurrent = dtgKhoa.SelectedItem as DataRowView;
 
+            if (_rowCurrent != null)
+            {
+                if (MessageBox.Show("Are you sure Delete?", "Delete Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+                    _idkhoa = Convert.ToInt32(_rowCurrent["IDKhoa"]);
+                    DeleteCourse(_idkhoa);
+                    ClearDataTextbox(txtMaKhoa, txtTenKhoa, txtGhiChuKhoa);
+                }
+            }
         }
+
+        public void LoadDataToCourseGrid()
+        {
+            dtgKhoa.ItemsSource = CourseDAL.Instance.GetListCourse().DefaultView;
+        }
+
+        public void LoadDataToGrid()
+        {
+            LoadDataToCourseGrid();
+        }
+
+        public void ClearDataTextbox(params TextBox[] array)
+        {
+            foreach (TextBox item in array)
+            {
+                item.Text = "";
+            }
+        }
+
+        public void AddCourse(int makhoa, string tenkhoa , string ghichu = null)
+        {
+            if (CourseDAL.Instance.InsertCourse(makhoa, tenkhoa, ghichu))
+            {
+                MessageBox.Show("Thêm Thành Công");
+            }
+            else
+            {
+                MessageBox.Show("Thêm Không Thành Công\n Vui Lòng Kiểm Tra lại", "Thông Báo");
+            }
+
+            LoadDataToCourseGrid();
+        }
+
+        public void EditCourse(int idkhoa , int makhoa, string tenkhoa, string ghichu = null)
+        {
+            if (CourseDAL.Instance.UpdateCourse(idkhoa, makhoa, tenkhoa, ghichu))
+            {
+                MessageBox.Show("Cập Nhật Thành Công");
+            }
+            else
+            {
+                MessageBox.Show("Cập Nhật Không Thành Công\n Vui Lòng Kiểm Tra lại", "Thông Báo");
+            }
+
+            LoadDataToCourseGrid();
+        }
+
+        public void DeleteCourse(int idkhoa)
+        {
+            if (CourseDAL.Instance.DeleteCourse(idkhoa))
+            {
+                MessageBox.Show("Xóa Thành Công", "Thông Báo");
+            }
+            else
+            {
+                MessageBox.Show("Xóa không thành công do xảy ra lỗi", "Thông Báo");
+            }
+
+            LoadDataToCourseGrid();
+        }
+
     }
 }
